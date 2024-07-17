4c38bd288e2cb7759a82f7991cfe4ed3ef65e6f9
xtclirui <821342264@qq.com>
2024-07-17 23:53:15 +0800

    update

--- handlers/video/feed_videolist_handler.go
+++
@@ -1,7 +0,0 @@
-package video
-
-import "github.com/gin-gonic/gin"
-
-func FeedVideoListHandler(c *gin.Context) {
-
-}
--- models/baseresponse.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type BaseResponse struct {
-	StatusCode int32  `json:"statusCode"`
-	StatusMsg  string `json:"statueMsg,omitempty"`
-}
